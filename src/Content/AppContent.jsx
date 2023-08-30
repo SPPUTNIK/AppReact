6b7eb6a0976ca62d5d56f85c0f229d315e5b4175
Abdessamad El bouziady <abel-bou@c3r10p6.1337.ma>
2023-08-30 04:56:11 +0100

    last

--- src/Content/AppContent.jsx
+++ src/Content/AppContent.jsx
@@ -7,6 +7,27 @@ import {BiSearchAlt} from 'react-icons/bi';
 import ProfileSection from "../Sidebar/ProfileSection";
 import {IoNotificationsOutline} from 'react-icons/io5';
 
+//image
+import imgRobot from './style/robot 1.svg';
+import imgUser from './style/user.svg';
+
+//stars
+import {TbTopologyStar2} from 'react-icons/tb';
+import {TbTopologyStar3} from 'react-icons/tb';
+import {TbTopologyStarRing2} from 'react-icons/tb';
+import {TbTopologyStarRing3} from 'react-icons/tb';
+import {GiStarfighter} from 'react-icons/gi';
+import {PiStarOfDavidBold} from 'react-icons/pi';
+
+//Grade
+import {GiUpgrade} from 'react-icons/gi';
+import {GiSwissArmyKnife} from 'react-icons/gi';
+import {GiSpeedBoat} from 'react-icons/gi';
+import {GiPowerRing} from 'react-icons/gi';
+import {GiPirateCaptain} from 'react-icons/gi';
+import {GiCapeArmor} from 'react-icons/gi';
+import {GiPirateFlag} from 'react-icons/gi';
+
 function AppContent() {
   return (
     <div className='app-content'>
@@ -38,26 +59,126 @@ function AppContent() {
 
       <div className='app-main'>
         <div className="containers app-main__containers">
+
+
           <div className="container-dashboard chart">
-          Chart
-          <DounutChart className="donut"/>
-          <div className="detailsDiv">
-            <ul>
-              <li>Total Of Won Matches <span>44 Matches</span></li>
-              <li>Total Of Lost Matches <span>12 Matches</span></li>
-              <li>Total Canceled Matches <span>06 Matches</span></li>
-            </ul>
+            <div className="chartDiv">
+              <DounutChart className="donut"/>
+            </div>
           </div>
-          </div>
+
+
           <div className="container-dashboard progress">
             <h2>Progress</h2>
+            <div className='item'>
+              <span>Total Matches 72</span>
+              <div className="colored_div color-total"></div>
+            </div>
+            <div className='item'>
+              <span>Get it 179 Points</span>
+              <div className="colored_div color-get"></div>
+            </div>
+            <div className='item'>
+              <span>Nedded 50 Points</span>
+              <div className="colored_div color-nedded"></div>
+            </div>
           </div>
-          <div className="container-dashboard">hello</div>
-          <div className="container-dashboard">hello</div>
-          <div className="container-dashboard">hello</div>
+
+
+
+          <div className="container-dashboard rank-card">
+            <h2>Top Ranking</h2>
+
+            <span className='rate'>10%</span>
+
+            <div className="grade-player">
+
+              <div className="grade">
+                <GiUpgrade className='topo'/>
+              </div>
+
+              <div className="stars">
+                <TbTopologyStar2 className='topo-logy-star'/>
+              </div>
+
+            </div>
+
+          </div>
+
+
+          <div className="container-dashboard robot">
+
+            <div className="img-robot">
+              <img src={imgRobot} alt='Robot'/>
+            </div>
+
+            <div className="button-click">
+              <button>Play With Robot</button>
+            </div>
+            
+          </div>
+
+
+          <div className="container-dashboard history">
+            <h2>History</h2>
+
+            <div className="matches">
+
+              <div className="match-one">
+                <span>2</span>
+                <img src={imgUser} alt='userImage'/>
+                <span>VS</span>
+                <img src={imgUser} alt='userImage'/>
+                <span>1</span>
+              </div>
+              <div className="match-one">
+                <span>2</span>
+                <img src={imgUser} alt='userImage'/>
+                <span>VS</span>
+                <img src={imgUser} alt='userImage'/>
+                <span>1</span>
+              </div>
+
+              <div className="match-one">
+                <span>2</span>
+                <img src={imgUser} alt='userImage'/>
+                <span>VS</span>
+                <img src={imgUser} alt='userImage'/>
+                <span>1</span>
+              </div>
+
+
+
+              {/* <div className="match-one">
+
+              </div>
+
+              <div className="match-one">
+
+              </div> */}
+
+            </div>
+
+          </div>
+
+
           <div className="container-dashboard">hello</div>
         </div>
       </div>
+                {/* <TbTopologyStar2 className='topo-logy-star'/>
+                <TbTopologyStar3 className='topo-logy-star'/>
+                <TbTopologyStarRing2 className='topo-logy-star'/>
+                <TbTopologyStarRing3 className='topo-logy-star'/>
+                <GiStarfighter className='topo-logy-star'/>
+                <PiStarOfDavidBold className='topo-logy-star'/>
+
+                <GiUpgrade className='topo'/>
+                <GiSwissArmyKnife className='topo'/>
+                <GiSpeedBoat className='topo'/>
+                <GiPowerRing className='topo'/>
+                <GiPirateCaptain className='topo'/>
+                <GiCapeArmor className='topo'/>
+                <GiPirateFlag className='topo'/> */}
       
     </div>
   )
--- src/Content/Chart.jsx
+++ src/Content/Chart.jsx
@@ -1,44 +1,42 @@
+import React from 'react';
 import {
     Chart as ChartJS,
     ArcElement,
     Tooltip,
     Legend
 } from 'chart.js';
-
 import { Doughnut } from 'react-chartjs-2';
-
 ChartJS.register(
     ArcElement,
     Tooltip,
     Legend
 );
+function DounutChart() {
+  const data = {
+    labels: ["Total Of Won Matches", "Total Of Lose Matches", "Total Canceled Matches"],
+    datasets: [
+      {
+        data: [10, 22, 54],
+        backgroundColor: [
+          "#00A3FF",
+          "#0CA593",
+          "#501AE9",
+        ],
+        hoverBackgroundColor: ["#00A3FF", "#0CA593", "#501AE9"],
+      },
+    ],
+  };
 
+  const options = {
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom",
+      },
+    },
+  };
 
-function DounutChart(){
-    const data = {
-        labels: [],
-        datasets: [{
-            label: 'User1',
-            data: [10, 22, 54],
-            backgroundColor: ['#00A3FF', '#0CA593', '#501AE9'],
-            borderColor: ['#00A3FF', '#0CA593', '#501AE9']
-        }]
-    }
-
-    const options = {
-        borderWidth: 10,
-        borderRadius: 2,
-        hoverBorderWidth: 0,
-        plugins: {
-            legend: {
-                display: false,
-            },
-        },
-
-    }
-    return (
-        <Doughnut data={data} options={options}/>
-    );
+  return <Doughnut data={data} options={options} />;
 }
 
 export default DounutChart;
